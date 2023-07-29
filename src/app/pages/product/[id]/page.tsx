7862e1966c18216948551db3c0b1a75fb64e4bf5
Brian Nguyen <briannguyen.sd@gmail.com>
2023-07-29 19:04:20 -0400

    Add product descriptions for product page

--- src/app/pages/product/[id]/page.tsx
+++ src/app/pages/product/[id]/page.tsx
@@ -21,6 +21,24 @@ const ProductPage = async ({ params }: { params: { id: string } }) => {
         <Gallery {...product} />
         <AddToCart {...product} />
       </div>
+      <div className={styles.productDescription}>
+        <div>
+          <h2>Description</h2>
+          <p>{product.description}</p>
+          <ul>
+            {product.features.map((feature: string) => (
+              <li>{feature}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <img
+            src={`/desktop/products/rect/product-${product.id}/3.png`}
+            alt={`${product.name} secondary style`}
+            className="image-format"
+          />
+        </div>
+      </div>
     </div>
   );
 };
